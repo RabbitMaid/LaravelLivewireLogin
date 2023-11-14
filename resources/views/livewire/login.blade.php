2134bd3e12a621134f9221a67a8458c96e95f058
Agbortoko <arreyagbortoko@gmail.com>
2023-11-14 18:07:47 +0100

    Auth working progress...

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,20 +1,20 @@
 
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 
-        <div class="w-50 mx-auto px-11 md:px-0">
+        <div class="w-50 mx-auto p-8 bg-white shadow-md">
 
             <h1 class="mb-3 text-2xl text-center">Login</h1>
 
             <form wire:submit.prevent="auth">
                 @csrf
 
-                <input type="email" wire:model="email" placeholder="Email" class="min-w-full mb-2 p-2 border border-slate-600 mx-auto">
+                <input type="email" wire:model="email" placeholder="Email" class="min-w-full mb-3 p-2 border border-slate-600 mx-auto">
 
                 @error('email')
                     <span class="text-red-800 inline-block mb-3">{{ $message }}</span>
                 @enderror
 
-                <input type="password" wire:model="password" placeholder="Password" class="min-w-full mb-2 p-2 border border-slate-600">
+                <input type="password" wire:model="password" placeholder="Password" class="min-w-full mb-3 p-2 border border-slate-600">
 
                 @error('password')
                 <span class="text-red-800 inline-block mb-3">{{ $message }}</span>
