7a34346ac986decf72812efe0488ff97b4daeef1
Agbortoko <arreyagbortoko@gmail.com>
2023-11-14 17:04:32 +0100

    Ongoing Auth Process

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,16 +2,35 @@
 
 namespace App\Livewire;
 
+
 use Livewire\Component;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class Login extends Component
 {
     public string $email;
     public string $password;
+    public bool $success;
 
 
-    public function login()
+    public function __construct()
     {
+        $this->success = false;
+    }
+
+    public function auth(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required|min:8'
+        ]);
+
+
+        if($validated){
+            $this->success = true;
+        }
+
 
     }
 
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,18 +1,33 @@
 
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 
-        <h1 class="mb-3 text-2xl">Login</h1>
+        <div class="w-50 mx-auto px-11 md:px-0">
 
-        <form wire:submit.prevent="login">
-            @csrf
+            @if($success)
+                <span class="text-red-800 inline-block mb-3">{{ $message }}</span>
+            @endif
+            <h1 class="mb-3 text-2xl">Login</h1>
 
-            <input type="email" wire:model="email" placeholder="Email" class="w-full mb-4 p-2 border border-slate-600">
+            <form wire:submit.prevent="auth">
+                @csrf
+
+                <input type="email" wire:model="email" placeholder="Email" class="min-w-full mb-2 p-2 border border-slate-600 mx-auto">
+
+                @error('email')
+                    <span class="text-red-800 inline-block mb-3">{{ $message }}</span>
+                @enderror
+
+                <input type="password" wire:model="password" placeholder="Password" class="min-w-full mb-2 p-2 border border-slate-600">
+
+                @error('password')
+                <span class="text-red-800 inline-block mb-3">{{ $message }}</span>
+                @enderror
+
+                <button type="submit" class="block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+            </form>
 
 
-            <input type="password" wire:model="password" placeholder="Password" class="w-full mb-4 p-2 border border-slate-600">
-
-            <button type="submit" class="block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
-        </form>
+        </div>
 
 
     </div>
